ac5ce9ca86e8617fd324b09654e37cb05f3fe0f8
Lorena Vilaça <lcjbv2@cin.ufpe.br>
2022-09-12 16:21:54 -0300

    Organizando pastas e criando tabBarViewController

---
+++ Receitinha/Receitinha/Controllers/PesquisarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PesquisarViewController.swift
+//  Receitinha
+//
+//  Created by LoreVilaca on 12/09/22.
+//
+
+import UIKit
+
+class PesquisarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Pesquisar"
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Receitinha/Receitinha/SceneDelegate.swift
+++ Receitinha/Receitinha/SceneDelegate.swift
@@ -18,7 +18,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController ()
+        let tabBarController = UITabBarController ()
+        let searchNavigation = UINavigationController (rootViewController: PesquisarViewController())
+        let recipeNavigation = UINavigationController (rootViewController: ReceitasViewController())
+        let conquerNavigation = UINavigationController (rootViewController: ConquistasViewController ())
+        tabBarController.viewControllers = [conquerNavigation, recipeNavigation, searchNavigation]
+        window.rootViewController = tabBarController
         self.window = window
         window.makeKeyAndVisible ()
     }
