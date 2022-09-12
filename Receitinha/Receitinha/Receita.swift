5415a50195cdde9a745af8c2e62cb220eb713d15
ditthales <thalesvgfraga@gmail.com>
2022-09-12 15:44:17 -0300

    Adicionando o modelo

---
+++ Receitinha/Receitinha/Receita.swift
@@ -0,0 +1,24 @@
+//
+//  Receita.swift
+//  ReceitinhaTeste
+//
+//  Created by ditthales on 12/09/22.
+//
+
+import Foundation
+
+struct Receita{
+    //Dados estáticos
+    let titulo: String
+    let dificuldade: String
+    let descricaoPrevia: String
+    let duracao: Int
+    let objetos: [String]
+    let ingredientes: [String]
+    //Dados que mudam por passo
+    let imagem: [String]
+    let tituloInstrucao: [String]
+    let descricao: [String]
+    let audioDescricao: [String]
+}
+
