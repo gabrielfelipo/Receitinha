f5945317dcbe9d17a27acbf973d5384abe991a54
Rinaldo Jr <102991159+RinaldoJr3@users.noreply.github.com>
2022-09-14 15:06:07 -0300

    Algumas configurações para começar a TableView

---
+++ Receitinha/Receitinha/Controllers/CheckViewController.swift
@@ -0,0 +1,93 @@
+//
+//  CheckViewController.swift
+//  Receitinha
+//
+//  Created by rsbj on 14/09/22.
+//
+
+import UIKit
+
+class CheckViewController: UIViewController {
+
+    lazy var collectionView:UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout.init())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .red
+        collectionView.register(CheckCollectionViewController.self, forCellWithReuseIdentifier: "Cell")
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout.init()
+        layout.scrollDirection = .vertical
+        collectionView.setCollectionViewLayout(layout, animated: false)
+        
+//        collectionView.delegate = self
+//        collectionView.dataSource = self
+        
+        return collectionView
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.isScrollEnabled = false
+//        tableView.delegate = self
+//        tableView.dataSource = self
+        tableView.register(CheckTableViewCell.self, forCellReuseIdentifier: CheckTableViewCell.indentifier)
+        return tableView
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(named: "blueBackground")
+        self.view.addSubview(self.tableView)
+        self.configConstraints()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func configConstraints(){
+        NSLayoutConstraint.activate([
+            
+            self.tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.tableView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.tableView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        
+        ])
+    }
+}
+
+extension CheckViewController:UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CheckTableViewCell.indentifier, for: indexPath) as? CheckTableViewCell
+        return cell ?? UITableViewCell()
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+}
+
+
+
+
+
+// MARK: - Preview
+#if DEBUG
+import SwiftUI
+@available(iOS 13, *)
+struct CheckViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        // view controller using programmatic UI
+        Group {
+            CheckViewController().showPreview().previewDevice("iPhone SE (3rd generation)")
+            CheckViewController().showPreview().previewDevice("iPhone SE (3rd generation)").previewInterfaceOrientation(.landscapeLeft)
+        }
+    }
+}
+#endif
+
