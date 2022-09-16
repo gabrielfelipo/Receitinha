d3d472e6065f69a1d139c92aea66a7e9862ae03f
gabrielfelipo <gfrm@cin.ufpe.br>
2022-09-16 17:56:20 -0300

    Search pesquisando e alterando a table view

--- Receitinha/Receitinha/Controllers/PesquisarViewController.swift
+++ Receitinha/Receitinha/Controllers/PesquisarViewController.swift
@@ -7,18 +7,82 @@
 
 import UIKit
 
-class PesquisarViewController: UIViewController {
+let listaReceitas = ["banana com nescau", "misto quente", "bolo", "bolacha"]
 
+var quantidadeCell = 0
+
+let pesquisarView = PesquisarView()
+
+class ResultsViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view = pesquisarView
+        
+    }
+}
+
+
+class PesquisarViewController: UIViewController, UISearchResultsUpdating, UITableViewDelegate, UITableViewDataSource {
+    
+    let searchController = UISearchController(searchResultsController: ResultsViewController())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTableView()
         setupPesquisarViewController()
-        // Do any additional setup after loading the view.
+    }
+    
+    func setupTableView() {
+        pesquisarView.receitasTableView.delegate = self
+        pesquisarView.receitasTableView.dataSource = self
     }
     
     func setupPesquisarViewController () {
         view.backgroundColor = UIColor(named: "blueBackground")
         self.navigationItem.title = "Buscar receita"
+        
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
     }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else {
+            return
+        }
+        
+        let viewController = searchController.searchResultsController as? ResultsViewController
+        viewController?.view.backgroundColor = UIColor(named: "blueBackground")
+        
+        print(text)
+        var listaResultado: [String] = []
+        
+        
+        if let foo = listaReceitas.enumerated().first(where: {$0.element.contains(text.lowercased())}) {
+           // do something with foo
+            for titulo in listaReceitas {
+                if titulo.contains(text.lowercased()) {
+                    listaResultado.append(titulo)
+                }
+                quantidadeCell = listaResultado.count
+                pesquisarView.receitasTableView.reloadData()
+            }
+            
+        }
+        
+        print(listaResultado)
+        //print(teste)
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quantidadeCell
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = pesquisarView.receitasTableView.dequeueReusableCell(withIdentifier: ReceitasTableViewCell.recipeCellIdentifier, for: indexPath) as! ReceitasTableViewCell
+            cell.backgroundColor = UIColor(named: "blueBackground")
+            return cell
+        }
 }
 
 
--- Receitinha/Receitinha/View/PesquisarView.swift
+++ Receitinha/Receitinha/View/PesquisarView.swift
@@ -8,13 +8,45 @@
 import UIKit
 
 class PesquisarView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    let receitasTableView: UITableView = {
+        let table = UITableView ()
+        table.separatorColor = .clear
+        table.backgroundColor = UIColor(named: "blueBackground")
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.showsVerticalScrollIndicator = false
+        return table
+    } ()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupRecipeViewHierarchy()
+        setupRecipeViewsAtributes()
+        setupRecipeConstraints()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not implemented")
+    }
+    
+    private func setupRecipeViewHierarchy () {
+        receitasTableView.register (ReceitasTableViewCell.self, forCellReuseIdentifier: ReceitasTableViewCell.recipeCellIdentifier)
+        self.addSubview(receitasTableView)
+        
+        
+    }
+    
+    private func setupRecipeViewsAtributes() {
+        //self.backgroundColor = UIColor(named: "blueBackground")
+    }
+    
+    private func setupRecipeConstraints () {
+        NSLayoutConstraint.activate([
+            receitasTableView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 16),
+            receitasTableView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            receitasTableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            receitasTableView.leadingAnchor.constraint(equalTo: self.leadingAnchor)
+            ])
+        
+    }
 }
